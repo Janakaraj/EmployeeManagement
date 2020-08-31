d933868357c0a4512ac6e3e3a8e9f09f234e3a52
Janakaraj <janak@promactinfo.com>
2020-08-31 19:47:56 +0530

    Dapper integration in employee

---
+++ EmployeeManagementApp/Models/EmployeeDataList.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployeeManagementApp.Models
+{
+    public class EmployeeDataList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public string Qualification { get; set; }
+        public long ContactNumber { get; set; }
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
--- EmployeeManagementApp/Models/EmployeeList.cs
+++ EmployeeManagementApp/Models/EmployeeList.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Data.SqlClient;
+using Dapper;
 using System;   
 namespace EmployeeManagementApp.Models
 {
@@ -16,89 +17,47 @@ namespace EmployeeManagementApp.Models
         }
         public static List<Employee> GetEmployees()
         {
-            con.Open();
-            SqlCommand cmd = new SqlCommand("select * from dbo.Employee inner join  dbo.Department on Employee.DepartmentId = Department.DepartmentId", con);
-            SqlDataReader reader = cmd.ExecuteReader();
             employeeList.Clear();
-            while (reader.Read())
+            SqlCommand cmd = new SqlCommand("select * from dbo.Employee inner join  dbo.Department on Employee.DepartmentId = Department.DepartmentId", con);
+            var employees = con.Query<EmployeeDataList>("select * from dbo.employee inner join  dbo.department on employee.departmentid = department.departmentid");
+            foreach (var emp in employees)
             {
                 Employee employee = new Employee();
-                employee.Id = Convert.ToInt32(reader[0]);
-                employee.Name = reader[1].ToString();
-                employee.Surname = reader[2].ToString();
-                employee.Address = reader[3].ToString();
-                employee.Qualification = reader[4].ToString();
-                employee.ContactNumber = long.Parse(Convert.ToString(reader[5]));
-                employee.DepartmentId = Convert.ToInt32(reader[6]);
+                employee.Id = Convert.ToInt32(emp.Id);
+                employee.Name = emp.Name.ToString();
+                employee.Surname = emp.Surname.ToString();
+                employee.Address = emp.Address.ToString();
+                employee.Qualification = emp.Qualification.ToString();
+                employee.ContactNumber = long.Parse(Convert.ToString(emp.ContactNumber));
+                employee.DepartmentId = Convert.ToInt32(emp.DepartmentId);
                 employee.department = new Department();
-                employee.department.DepartmentId = Convert.ToInt32(reader[7]);
-                employee.department.DepartmentName = reader[8].ToString();
+                employee.department.DepartmentId = Convert.ToInt32(emp.DepartmentId);
+                employee.department.DepartmentName = emp.DepartmentName.ToString();
                 employeeList.Add(employee);
             }
-            con.Close();
-            //var employees = con.Query<EmployeeDataList>("select * from dbo.Employee inner join  dbo.Department on Employee.DepartmentId = Department.DepartmentId");
-            //foreach (var emp in employees)
-            //{
-            //    Employee employee = new Employee();
-            //    employee.Id = Convert.ToInt32(emp.Id);
-            //    employee.Name = emp.Name.ToString();
-            //    employee.Surname = emp.Surname.ToString();
-            //    employee.Address = emp.Address.ToString();
-            //    employee.Qualification = emp.Qualification.ToString();
-            //    employee.ContactNumber = long.Parse(Convert.ToString(emp.ContactNumber));
-            //    employee.DepartmentId = Convert.ToInt32(emp.DepartmentId);
-            //    employee.department = new Department();
-            //    employee.department.DepartmentId = Convert.ToInt32(emp.DepartmentId);
-            //    employee.department.DepartmentName = emp.DepartmentName.ToString();
-            //    employeeList.Add(employee);
-            //}
             return employeeList;
         }
         public static Employee AddToList(Employee employee)
         {
             employeeList.Add(employee);
-            con.Open();
-            string query = "INSERT INTO dbo.Employee(Name, Surname, Address, Qualification, ContactNumber, DepartmentId) VALUES(@Name, @Surname, @Address, @Quali, @Cn, @Dept)";
-            SqlCommand cmd = new SqlCommand(query, con);
-            cmd.Parameters.AddWithValue("@Name", employee.Name);
-            cmd.Parameters.AddWithValue("@Surname", employee.Surname);
-            cmd.Parameters.AddWithValue("@Address", employee.Address);
-            cmd.Parameters.AddWithValue("@Quali", employee.Qualification);
-            cmd.Parameters.AddWithValue("@Cn", employee.ContactNumber);
-            cmd.Parameters.AddWithValue("@Dept", employee.DepartmentId);
-            cmd.ExecuteNonQuery();
-            con.Close();
-            //var name = employee.Name;
-            //var surname = employee.Surname;
-            //var address = employee.Address;
-            //var quali = employee.Qualification;
-            //var cn = employee.ContactNumber;
-            //var DId = employee.DepartmentId;
-            //con.Execute("INSERT INTO dbo.Employee(Name, Surname, Address, Qualification, ContactNumber, DepartmentId) VALUES(name, surname, address, quali, cn, DId)");
+            string query = "INSERT INTO dbo.Employee(Name, Surname, Address, Qualification, ContactNumber, DepartmentId) VALUES(" + "'" + employee.Name + "'" + ","
+                + "'" + employee.Surname + "'" + "," + "'" + employee.Address + "'" + "," + "'" + employee.Qualification + "'" + "," + "'" + employee.ContactNumber + "'" + "," + "'" + employee.DepartmentId + "'" + ")";
+            con.Execute(query);
             return employee;
         }
         public static Employee EditInList(int id, Employee employee)
         {
             employeeList.Clear();
-            con.Open();
             string query = "UPDATE dbo.Employee SET Name = '" + employee.Name + "', Surname = '" + employee.Surname + "', Address = '" + employee.Address + "', Qualification = '" + employee.Qualification + "', ContactNumber = '" + employee.ContactNumber + "', DepartmentId = '" + employee.DepartmentId + "' WHERE Id = " + id;
-            SqlCommand cmd = new SqlCommand(query, con);
-            cmd.ExecuteNonQuery();
-            con.Close();
+            con.Execute(query);
             
             return employee;
         }
         public static void DeleteInList(int id)
         {
-            //Employee employeeToDelete = employeeList.Find(x => x.Id == id);
-            //employeeList.Remove(employeeToDelete);
-            //return employeeToDelete;
             employeeList.Clear();
-            con.Open();
             string query = "DELETE FROM dbo.Employee WHERE Id = " + id;
-            SqlCommand cmd = new SqlCommand(query, con);
-            cmd.ExecuteNonQuery();
-            con.Close();
+            con.Execute(query);
         }
     }
 }
