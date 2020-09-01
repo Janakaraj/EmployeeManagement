51c1d39d3e9acdc2900703cc0805844b4b9f68da
Janakaraj <69618997+Janakaraj@users.noreply.github.com>
2020-09-01 20:06:04 +0530

    Update Employee.sql

--- EmployeeManagementDB/dbo/Tables/Employee.sql
+++ EmployeeManagementDB/dbo/Tables/Employee.sql
@@ -1,11 +1,11 @@
-﻿CREATE TABLE [dbo].[employees] (
-    [Id]            INT            IDENTITY (1, 1) NOT NULL,
-    [Name]          NVARCHAR (20)  NOT NULL,
-    [Surname]       NVARCHAR (20)  NOT NULL,
-    [Address]       NVARCHAR (100) NOT NULL,
-    [Qualification] NVARCHAR (MAX) NOT NULL,
-    [ContactNumber] BIGINT         NOT NULL,
-    [DepartmentId]  INT            NOT NULL,
-    CONSTRAINT [PK_employees] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_employees_depatments_DepartmentId] FOREIGN KEY ([DepartmentId]) REFERENCES [dbo].[depatments] ([DepartmentId]) ON DELETE CASCADE
-);
+CREATE TABLE dbo.Employee
+(
+	Id INT NOT NULL PRIMARY KEY, 
+    Name NVARCHAR(20) NOT NULL, 
+    Surname NVARCHAR(20) NOT NULL, 
+    Address NVARCHAR(100) NOT NULL, 
+    Qualification NVARCHAR(MAX) NOT NULL, 
+    ContactNumber BIGINT NOT NULL, 
+    DepartmentId INT NOT NULL,
+    FOREIGN KEY (DepartmentId) REFERENCES dbo.Department(DepartmentId) 
+)
